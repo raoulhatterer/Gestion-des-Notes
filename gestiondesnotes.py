fbbe3a7d79dfcf4d393448095f6b8fae4d7bcbf0
raoulhatterer <hatterer.raoul@gmail.com>
2020-02-27 04:38:23 +0100

    Ajout d'enseignants

--- gestiondesnotes.py
+++ gestiondesnotes.py
@@ -2,14 +2,12 @@
 # author: Raoul HATTERER
 # date: fevrier 2020
 
-
-import mysql.connector 
+import mysql.connector
 from mysql.connector import Error
 import tkinter as tk
 from tkinter import ttk
 # installer tksheet avec pip
 from tksheet import Sheet
-
 
 # pour debugger
 # import pdb
@@ -19,114 +17,26 @@ from tksheet import Sheet
 
 # Au préalable:
 # -------------
-# créer la base de données et les utlisateurs stil et gestionnaire 
+# créer la base de données et les utlisateurs stil et gestionnaire
 # et initialiser les tables de la BD grâce au script initdb_gestiondesnotes.SQL
 # par la commande
 # source ./initdb_gestiondesnotes.SQL
 
 
-class demo(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.grid_columnconfigure(0, weight = 1)
-        self.grid_rowconfigure(0, weight = 1)
-        self.sheet_demo = Sheet(self,
-                                data = [[f"{r}, {c}" for c in range(30)] for r in range(2000)], #to set sheet data at startup
-                                #set_all_heights_and_widths = True, #to fit all cell sizes to text at start up
-                                #headers = 0, #to set headers as first row at startup
-                                #row_index = 0, #to set row_index as first column at startup
-                                #total_rows = 2000, #if you want to set empty sheet dimensions at startup
-                                #total_columns = 30, #if you want to set empty sheet dimensions at startup
-                                height = 500, #height and width arguments are optional
-                                width = 700) #For full startup arguments/parameters see DOCUMENTATION.md
-        self.sheet_demo.enable_bindings(("single_select", #"single_select" or "toggle_select"
-                                         "drag_select",   #enables shift click selection as well
-                                         "column_drag_and_drop",
-                                         "row_drag_and_drop",
-                                         "column_select",
-                                         "row_select",
-                                         "column_width_resize",
-                                         "double_click_column_resize",
-                                         "row_width_resize",
-                                         "column_height_resize",
-                                         "arrowkeys",
-                                         "row_height_resize",
-                                         "double_click_row_resize",
-                                         "right_click_popup_menu",
-                                         "rc_select",
-                                         "rc_insert_column",
-                                         "rc_delete_column",
-                                         "rc_insert_row",
-                                         "rc_delete_row",
-                                         "copy",
-                                         "cut",
-                                         "paste",
-                                         "delete",
-                                         "undo",
-                                         "edit_cell"))
-        #self.sheet_demo.enable_bindings("enable_all")
-        #self.sheet_demo.disable_bindings() #uses the same strings
-        self.sheet_demo.grid(row = 0, column = 0, sticky = "nswe")
-        
+# ------------------------------------------------------------------------------
+# DISCIPLINES
+# ------------------------------------------------------------------------------
 
-
-
-try:
-    print("Try to connected to MySQL Server")
-    connection = mysql.connector.connect(host='localhost',
-                                         database='bd_gestion_des_notes',
-                                         user='user_gestionnaire',
-                                         password='gestionnaire')
-    db_Info = connection.get_server_info()
-    print("Connected to MySQL Server version", db_Info)
-    cursor = connection.cursor()
-    cursor.execute("select database();")
-    record = cursor.fetchone()
-    print("You are connected to database: ", record)
-
-    cursor = connection.cursor()
-    cursor.execute("show databases;")
-    records = cursor.fetchall()
-    print("Databases: ", records)
-
-    cursor = connection.cursor()
-    cursor.execute("show tables;")
-    records = cursor.fetchall()
-    print("All tables: ", records)
-
-    cursor = connection.cursor()
-    cursor.execute("desc Professeurs;")
-    records = cursor.fetchall()
-    print("All attributes of Professeurs (", cursor.rowcount, "): ")
-    for row in records:
-        print("\t", row)
-
-    cursor = connection.cursor()
-    cursor.execute("select * from Professeurs;")
-    records = cursor.fetchall()
-    print("All professeur of Professeurs (", cursor.rowcount, "): ")
-    for row in records:
-        print("\t", row)
-
-    cursor = connection.cursor()
-    cursor.execute("desc Disciplines;")
-    records = cursor.fetchall()
-    print("All attributes of Disciplines (", cursor.rowcount, "): ")
-    for row in records:
-        print("\t", row)
-
-
-        
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if (connection.is_connected()):
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
-
-
-def charger_disciplines():
+def _charger_disciplines():
+    """
+    Fonction appelée par 'afficher_disciplines()'
+    Retourne un tupple conteant 'disc_Id' et 'disc_Name'.
+    disc_Id : liste contenant tous les index générés de façon automatique de
+    la table Discipline de la base de donnée 'bd_gestion_des_notes'.
+    disc_Name : liste contenant tous les noms des disciplines (dans l'ordre
+    alphabétique) contenus dans la table Discipline de la base de données
+    'bd_gestion_des_notes'.
+    """
     try:
         print("Try to connected to MySQL Server")
         connection = mysql.connector.connect(host='localhost',
@@ -140,14 +50,11 @@ def charger_disciplines():
         records = cursor.fetchall()
         print(records)
         print("All discipline of Disciplines (", cursor.rowcount, "): ")
-        Disc_Id, Disc_Name = list(), list()
-        index = 0
+        disc_Id, disc_Name = list(), list()
         for row in records:
             print("\t", row)
-            Disc_Id += [[row[0]]]
-            Disc_Name += [[row[1]]]
-        print(Disc_Id)
-        print(Disc_Name)
+            disc_Id += [[row[0]]]
+            disc_Name += [[row[1]]]
     except Error as e:
         print("Error while connecting to MySQL", e)
     finally:
@@ -155,17 +62,25 @@ def charger_disciplines():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
-    return (Disc_Id, Disc_Name)
+    return (disc_Id, disc_Name)
+
 
 def afficher_disciplines():
-    global sheet_disciplines, Disc_Id, Disc_Name
-    Disc_Id, Disc_Name = charger_disciplines()
+    """
+    Affiche un tableau de type tableur avec toutes les disciplines
+    """
+    global sheet_disciplines, disc_Id, disc_Name
+    disc_Id, disc_Name = _charger_disciplines()
     sheet_disciplines = Sheet(f1,
-                         data = Disc_Name, #to set sheet data at startup
-                         height = 600, 
-                         width = 800)
-    sheet_disciplines.grid(row = 0, column = 0, columnspan=2, sticky = "nswe")
-    sheet_disciplines.enable_bindings(("single_select", #"single_select" or "toggle_select"
+                              data=disc_Name,  # to set sheet data at startup
+                              height=600,
+                              width=800)
+    # sheet_disciplines.hide("row_index")
+    sheet_disciplines.hide("top_left")
+    sheet_disciplines.hide("header")
+    sheet_disciplines.grid(row=0, column=0, columnspan=2, sticky="nswe")
+    sheet_disciplines.enable_bindings(("single_select",  # "single_select" or "toggle_select"
+                                       "arrowkeys",
                                        "copy",
                                        "cut",
                                        "paste",
@@ -173,13 +88,43 @@ def afficher_disciplines():
                                        "undo",
                                        "edit_cell"))
 
+def enregistrer_disciplines():
+    """
+    Met à jour la base de donnée en y ajoutant d'éventuelles nouvelles
+    disciplines.
+    """
+    try:
+        print("Try to connected to MySQL Server")
+        connection = mysql.connector.connect(host='localhost',
+                                             database='bd_gestion_des_notes',
+                                             user='user_gestionnaire',
+                                             password='gestionnaire')
+        cursor = connection.cursor()
+        for index in range(len(disc_Name)):
+            sql = "UPDATE Disciplines SET Disc_Name = %s WHERE Disc_Id = %s"
+            discipline = (disc_Name[index][0], disc_Id[index][0])
+            cursor.execute(sql, discipline)
+            connection.commit()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    sheet_disciplines.destroy()
+    afficher_disciplines()
 
-    
 
 def ajouter_discipline():
+    """
+    Ajoute une 'Nouvelle discipline' dans le tableau des disciplines (et dans
+    la base de donnée) et sélectionne la ligne. Commence par enregistrer
+    l'état précédent du tableau dans la base de donnée dans le cas où
+    l'utilisateur enchaîne les ajouts.
+    """
+    enregistrer_disciplines()
     global sheet_disciplines
-    #sheet_disciplines.insert_row() # an empty row at the end
-    #sheet_disciplines.refresh()
     sql = "INSERT INTO Disciplines (Disc_Name) VALUES ('Nouvelle Discipline')"
     try:
         print("Try to connected to MySQL Server")
@@ -199,12 +144,84 @@ def ajouter_discipline():
             print("MySQL connection is closed")
     sheet_disciplines.destroy()
     afficher_disciplines()
+    sheet_disciplines.select_row(disc_Name.index(['Nouvelle Discipline']))
 
 
-def cell_select(self, response):
-    print (response)
+# ------------------------------------------------------------------------------
+# PROFESSEURS
+# ------------------------------------------------------------------------------
 
-def enregistrer_disciplines():
+def _charger_professeurs():
+    """
+    Fonction appelée par 'afficher_professeurs()'
+    Retourne un tupple conteant 'disc_Id' et 'disc_Name'.
+    disc_Id : liste contenant tous les index générés de façon automatique de
+    la table Discipline de la base de donnée 'bd_gestion_des_notes'.
+    disc_Name : liste contenant tous les noms des professeurs (dans l'ordre
+    alphabétique) contenus dans la table Discipline de la base de données
+    'bd_gestion_des_notes'.
+    """
+    try:
+        print("Try to connected to MySQL Server")
+        connection = mysql.connector.connect(host='localhost',
+                                             database='bd_gestion_des_notes',
+                                             user='user_gestionnaire',
+                                             password='gestionnaire')
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select Prof_Id, FirstName, LastName, Disc_Id, Gender from Professeurs ORDER BY LastName ASC;")
+        records = cursor.fetchall()
+        print(records)
+        print("All discipline of Professeurs (", cursor.rowcount, "): ")
+        prof_id, prof_print = list(), list()
+        for row in records:
+            print("\t", row)
+            prof_id += [row[0]]
+            if row[4] == 'M':
+                prof_print += [['M', row[1], row[2], row[3]]]
+            elif row[4] == 'F':
+                prof_print += [['Mme', row[1], row[2], row[3]]]
+            else:
+                prof_print += [['?', row[1], row[2], row[3]]]                
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    return (prof_id, prof_print)
+
+
+def afficher_professeurs():
+    """
+    Affiche un tableau de type tableur avec toutes les professeurs
+    """
+    global sheet_professeurs, prof_Id, prof_print
+    prof_Id, prof_print = _charger_professeurs()
+    sheet_professeurs = Sheet(f2,
+                              data=prof_print,  # to set sheet data at startup
+                              height=600,
+                              width=800)
+    # sheet_professeurs.hide("row_index")
+    sheet_professeurs.hide("top_left")
+    sheet_professeurs.hide("header")
+    sheet_professeurs.grid(row=0, column=0, columnspan=2, sticky="nswe")
+    sheet_professeurs.enable_bindings(("single_select",  # "single_select" or "toggle_select"
+                                       "arrowkeys",
+                                       "copy",
+                                       "cut",
+                                       "paste",
+                                       "delete",
+                                       "undo",
+                                       "edit_cell"))
+
+
+def enregistrer_professeurs():
+    """
+    Met à jour la base de donnée en y ajoutant d'éventuelles modifications.
+    """
     try:
         print("Try to connected to MySQL Server")
         connection = mysql.connector.connect(host='localhost',
@@ -212,11 +229,14 @@ def enregistrer_disciplines():
                                              user='user_gestionnaire',
                                              password='gestionnaire')
         cursor = connection.cursor()
-        for index in range(len(Disc_Name)):
-            sql = "UPDATE Disciplines SET Disc_Name = %s WHERE Disc_Id = %s"
-            discipline = (Disc_Name[index][0], Disc_Id[index][0])
-            print(discipline)
-            cursor.execute(sql, discipline)
+        for index in range(len(disc_Name)):
+            sql = "UPDATE Professeurs SET FirstName=%s, LastName=%s, Disc_Id=%s, Gender=%s WHERE Prof_Id = %s"
+            if prof_print[index][0] in ['M', 'M.']:
+                professeur = (prof_print[index][1], prof_print[index][2], prof_print[index][3], 'M', prof_Id[index])
+            else:
+                professeur = (prof_print[index][1], prof_print[index][2], prof_print[index][3], 'F', prof_Id[index])                
+            print(professeur)
+            cursor.execute(sql, professeur)
             connection.commit()
     except Error as e:
         print("Error while connecting to MySQL", e)
@@ -225,37 +245,75 @@ def enregistrer_disciplines():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
-    sheet_disciplines.destroy()
-    afficher_disciplines()
+    sheet_professeurs.destroy()
+    afficher_professeurs()
+
+
+def ajouter_professeur():
+    """
+    Ajoute une 'Nouvelle discipline' dans le tableau des professeurs (et dans
+    la base de donnée) et sélectionne la ligne. Commence par enregistrer
+    l'état précédent du tableau dans la base de donnée dans le cas où
+    l'utilisateur enchaîne les ajouts.
+    """
+    enregistrer_professeurs()
+    global sheet_professeurs
+    sql = "INSERT INTO Professeurs (FirstName, LastName) VALUES ('Prénom ?', 'Nom ?')"
+    try:
+        print("Try to connected to MySQL Server")
+        connection = mysql.connector.connect(host='localhost',
+                                             database='bd_gestion_des_notes',
+                                             user='user_gestionnaire',
+                                             password='gestionnaire')
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    sheet_professeurs.destroy()
+    afficher_professeurs()
+#    sheet_professeurs.select_row(prof_print.index(['Nouvel Enseignant']))
 
 
 
-root = tk.Tk()
+
+
+
+    
+def cell_select(self, response):
+    print(response)
+
+
+
+root=tk.Tk()
 root.title("Gestion des notes")
 root.resizable(False, False)
 style = ttk.Style(root)
 style.configure("lefttab.TNotebook", tabposition="n")
- 
+
 notebook = ttk.Notebook(root, style="lefttab.TNotebook")
- 
+
 f1 = tk.Frame(notebook, width=800, height=600)
 f2 = tk.Frame(notebook, width=800, height=600)
 f3 = tk.Frame(notebook, width=800, height=600)
 f4 = tk.Frame(notebook, width=800, height=600)
 
-
-
 afficher_disciplines()
+afficher_professeurs()
 
-
-#sheet_disciplines.hide("row_index")
-sheet_disciplines.hide("top_left")
-sheet_disciplines.hide("header")
-#sheet_disciplines.total_rows(4) #will delete rows if set to less than current data rows
-
-
-button_add_discipline = tk.Button(f1, text='Ajouter', command=ajouter_discipline)
-button_save_disciplines = tk.Button(f1, text='Enregistrer', command=enregistrer_disciplines)
+button_add_discipline = tk.Button(f1, text='Ajouter',
+                                  command=ajouter_discipline)
+button_save_disciplines = tk.Button(f1, text='Enregistrer',
+                                    command=enregistrer_disciplines)
+button_add_prof = tk.Button(f2, text='Ajouter',
+                                  command=ajouter_professeur)
+button_save_profs = tk.Button(f2, text='Enregistrer',
+                                    command=enregistrer_professeurs)
 
 notebook.add(f1, text="Disciplines")
 notebook.add(f2, text="Enseignants")
@@ -273,4 +331,6 @@ notebook.grid(row=0, column=0, sticky="nw")
 
 button_add_discipline.grid(row=1, column=0)
 button_save_disciplines.grid(row=1, column=1)
+button_add_prof.grid(row=1, column=0)
+button_save_profs.grid(row=1, column=1)
 root.mainloop()
